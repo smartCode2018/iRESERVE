c8b1754d162a3a1e0a7181e7bc794e7a8f27df72
Justice Kelechi <smartcode.kc@gmail.com>
2023-05-01 22:18:41 -0200

    creaed modal component

--- app/components/navbar/Usermenu.tsx
+++ app/components/navbar/Usermenu.tsx
@@ -3,7 +3,9 @@ import React, { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "../hooks/useRegisterModal";
 const Usermenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -77,7 +79,7 @@ const Usermenu = () => {
           >
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Sign Up" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
             </>
           </div>
         </div>
